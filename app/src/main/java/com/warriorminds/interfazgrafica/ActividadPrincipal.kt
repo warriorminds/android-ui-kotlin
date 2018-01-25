f1410e402066b12106279846c3ae5b3282483cc8
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-25 16:13:09 -0600

    Primer botón y creación de actividad para linear layout vertical.

--- app/src/main/java/com/warriorminds/interfazgrafica/ActividadPrincipal.kt
+++ app/src/main/java/com/warriorminds/interfazgrafica/ActividadPrincipal.kt
@@ -1,12 +1,24 @@
 package com.warriorminds.interfazgrafica
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.warriorminds.interfazgrafica.layouts.ActividadLinearLayoutVertical
+import kotlinx.android.synthetic.main.actividad_principal.*
 
 class ActividadPrincipal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actividad_principal)
+
+        botonLinearLayoutVertical.setOnClickListener {
+            abrirActividadLinearLayoutVertical()
+        }
+    }
+
+    private fun abrirActividadLinearLayoutVertical() {
+        val intent = Intent(this, ActividadLinearLayoutVertical::class.java)
+        startActivity(intent)
     }
 }
