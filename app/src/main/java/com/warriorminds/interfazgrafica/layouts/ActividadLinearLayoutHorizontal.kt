7b31e1ba83ebd6468c94a0e8e151b4955b0d6a80
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-25 16:58:20 -0600

    LinearLayout horizontal.

---
+++ app/src/main/java/com/warriorminds/interfazgrafica/layouts/ActividadLinearLayoutHorizontal.kt
@@ -0,0 +1,13 @@
+package com.warriorminds.interfazgrafica.layouts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.warriorminds.interfazgrafica.R
+
+class ActividadLinearLayoutHorizontal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad_linear_layout_horizontal)
+    }
+}
