d14fc56e010352d0a2befd76ae0056c2a1838862
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-25 15:26:01 -0600

    proyecto inicial

---
+++ app/src/main/java/com/warriorminds/interfazgrafica/ActividadPrincipal.kt
@@ -0,0 +1,11 @@
+package com.warriorminds.interfazgrafica
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
